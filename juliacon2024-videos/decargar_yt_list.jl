0e7f558e3b44af2ab2dea8b8acb88c6258f28753
Augusto Kielbowicz <augusto.kiel@gmail.com>
2024-07-15 01:35:48 -0300

    argentina campeon!

--- juliacon2024-videos/decargar_yt_list.jl
+++ juliacon2024-videos/decargar_yt_list.jl
@@ -34,22 +34,56 @@ using JSON3
 # talks_str = String(HTTP.get(talks_endpoint; query=["limit"=>500]).body)
 talks_str = open(io->read(io, String), "talks.json")
 talks = JSON3.read(talks_str)
-
 talks_info = collect(map(t-> Dict(:title=>t[:title],:room=>t[:slot][:room][:en], :start=> t[:slot][:start]), talks.results ))
-
 sort!(talks_info, by=t->(t[:room],t[:start]))
 # write("talks.json", talks_str)
 
-else_d1 = filter(t-> occursin("Else", t[:room]) && occursin("07-10",t[:start]), talks_info )
+by_room_str = open(io->read(io, String), "by_room.json")
+by_room = JSON3.read(by_room_str)
 
 to_dt(str) = parse(DateTime, split( str, "+02:00")[1], Dates.ISODateTimeFormat)
+# talk_name = Symbol("REPL (2, main stage)")
+# talk_name = Symbol("For Loop (3.2)")
 
-times = map(t-> to_dt(t[:start]), else_d1)
+room_name = first(filter(contains("Method")∘String, keys(by_room)))
+days = Dict(1=>"07-10",2=> "07-11",3=>"07-12")
+day = days[2]
+filtered_talks = filter(t-> occursin(day,t[:start]), by_room[room_name])
+
+
+times = map(t-> to_dt(t[:start]), filtered_talks)
 start_time = floor(times[1], Dates.Hour)
 
 # ####################
 using VideoIO
-file_name = "JuliaCon 2024 _ Else Room _ Day 1.mp4"
+
+vds = [
+  # ("JuliaCon 2024 | Else Room | Day 1", "https://www.youtube.com/watch?v=LNfSeco0q04")
+  # ("JuliaCon 2024 | If Room | Day 1", "https://www.youtube.com/watch?v=6ES-yZke5DQ")
+  # ("JuliaCon 2024 | Method Room | Day 1", "https://www.youtube.com/watch?v=1c4TTosBzzY")
+  # ("JuliaCon 2024 | For Loop Room | Day 1", "https://www.youtube.com/watch?v=HCSdOMnzcnY")
+  # ("JuliaCon 2024 | While Loop Room | Day 1", "https://www.youtube.com/watch?v=-2OkeCpRB-Y")
+  # ("JuliaCon 2024 | REPL Main Stage | Day 1", "https://www.youtube.com/watch?v=OQnHyHgs0Qo")
+# ("JuliaCon 2024 | Function Room | Day 1", "https://www.youtube.com/watch?v=V7FWl4YumcA")
+
+# ("JuliaCon 2024 | For Loop Room | Day 2", "https://www.youtube.com/watch?v=ZKt0tiG5ajw")
+# ("JuliaCon 2024 | Method Room | Day 2", "https://www.youtube.com/watch?v=gbtTJQFVijM")
+("JuliaCon 2024 | REPL Main Stage | Day 2", "https://www.youtube.com/watch?v=Bo3k4oD1Avg")
+("JuliaCon 2024 | Function Room | Day 2", "https://www.youtube.com/watch?v=v0RPD4eSzVE")
+("JuliaCon 2024 | While Loop Room | Day 2", "https://www.youtube.com/watch?v=PzgFzorRtoo")
+
+("JuliaCon 2024 | REPL Main Stage | Day 3", "https://www.youtube.com/watch?v=t6st0vo0hh4")
+
+("JuliaCon 2024 | Else Room | Day 3", "https://www.youtube.com/watch?v=wqkAz45AdXI")
+("JuliaCon 2024 | For Loop Room | Day 3", "https://www.youtube.com/watch?v=f7CLxthbZes")
+("JuliaCon 2024 | If Room | Day 3", "https://www.youtube.com/watch?v=Zd33ePxEavc")
+("JuliaCon 2024 | While Loop Room | Day 3", "https://www.youtube.com/watch?v=IgFF9JNU9cM")
+("JuliaCon 2024 | Method Room | Day 3", "https://www.youtube.com/watch?v=6Te9rThZaa4")
+("JuliaCon 2024 | Function Room | Day 3", "https://www.youtube.com/watch?v=qru5G5Yp77E")
+]
+
+filenames = map(s-> replace(s[1],"|"=>"_"), vds)
+file_name = "/mnt/d/$(filenames[1]).mp4"
 
 nframes = VideoIO.get_number_frames(file_name)
 duration = VideoIO.get_duration(file_name)
@@ -60,14 +94,18 @@ f = VideoIO.openvideo(file_name)
 # iterar sobre los frames skipeando cada 60 y calcular la similitud
 #
 i0 = read(f)
-baseIm = i0[440:640, 860:1060]
+# xrange, yrange = 440:640, 860:1060
+# xrange, yrange = 300:600, 1:500 # REPL d1
+# xrange, yrange = 1:144, 1:256# REPL d1
+xrange,yrange=150:250,250:400
+baseIm = i0[xrange, yrange]
 #
 
 using Images
 
 function get_frame_at_sec(i)
   seek(f, i)
-  im = read(f)[440:640, 860:1060]
+  im = read(f)[xrange, yrange]
   im
 end
 
@@ -86,7 +124,8 @@ end
 
 to_second(time, offset_min=10) = Int((time-start_time).value / 1000) + offset_min*60
 
-get_rango(time) = rango .+ to_second(time)
+offset = 60
+get_rango(time) = rango .+ to_second(time, offset)
 
 function process(t)
   try
@@ -99,7 +138,6 @@ function process(t)
   end
 end
 
-res = map(process, times[emp])
 
 
 function f2(r)
@@ -108,34 +146,27 @@ function f2(r)
   (r[1],x,y)
 end
 
-res_split = map(f2, res)
 
 find_peak(res) = res[2][findall( abs.(diff(res[3] .> 0.8)) .> 0 )]
 
-for (d,p) in zip(else_d1, peaks)
-  d[:peaks] = join(p,",")
-end
-
-
-open("my.json", "w") do f
-    JSON3.write(f, else_d1)
-    println(f)
-end
-
-video_link = "https://www.youtube.com/watch?v=LNfSeco0q04&t="
 
 using Printf
-function make_comment(talk)
-    f = first(split(talk[:peaks],","))
-    f = isempty(f) ? "0" : f 
-    peak = parse(Int,f)
+
+function make_comment(talk, peaks)
+    peak = isempty(peaks) ? 0 : Int(floor(mean(peaks)))
     h,m,s = sec_to_T(peak)
     m2 = @sprintf "%02d" m
     s2 = @sprintf "%02d" s+2
     "$h:$m2:$s2 - $(talk[:title])" 
 end
 
-comment(talks) = join(map(t->make_comment(t),talks),"\n")
+comment(talks, peaks) = join(map(t->make_comment(t...),zip(talks,peaks)),"\n")
+
+res = map(process, times)
+res_split = map(f2, res)
+peaks = map(find_peak, res_split)
+comment(filtered_talks, peaks) |> clipboard
 
 
-comment(video_link, else_d1) |> clipboard
+
+
